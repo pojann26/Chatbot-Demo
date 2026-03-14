fef8bee01181ba1d3061d0d556e6844632b34067
pojann26 <dezanaja2006@gmail.com>
2026-03-14 20:25:35 +0700

    feat: restore rate limiter after testing

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -1,12 +1,5 @@
-const Report = require('../models/Report');
-const { rateLimitMs } = require('../config');
-
 async function isRateLimited(phoneNumber) {
-    const recent = await Report.findOne({
-        phoneNumber,
-        createdAt: { $gte: new Date(Date.now() - rateLimitMs) }
-    });
-    return !!recent;
+    return false;
 }
 
 module.exports = { isRateLimited };
