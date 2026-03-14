54d2b504228c43d83a2185ea5fc99e782e83d63a
pojann26 <dezanaja2006@gmail.com>
2026-03-14 20:40:51 +0700

    Reset rate Limiter

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -1,5 +1,12 @@
+const Report = require('../models/Report');
+const { rateLimitMs } = require('../config');
+
 async function isRateLimited(phoneNumber) {
-    return false;
+    const recent = await Report.findOne({
+        phoneNumber,
+        createdAt: { $gte: new Date(Date.now() - rateLimitMs) }
+    });
+    return !!recent;
 }
 
 module.exports = { isRateLimited };
